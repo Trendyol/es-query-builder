2e26bbc40ab5a78fe08664e2daee9a2bae256712
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2024-06-01 03:23:55 +0300

    test: seperate benchmark tests

---
+++ benchmarks/aggs_example_test.go
@@ -0,0 +1,149 @@
+package benchmarks
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/GokselKUCUKSAHIN/es-query-builder/es"
+	"github.com/GokselKUCUKSAHIN/es-query-builder/es/enums/sort/order"
+	"github.com/GokselKUCUKSAHIN/es-query-builder/test/assert"
+)
+
+////    Aggs Example    ////
+
+func createAggsQuery() string {
+	query := es.NewQuery(
+		es.Bool().
+			Must(
+				es.Term("type", "File"),
+			).
+			MustNot(
+				es.Exists("file.name"),
+			),
+	)
+
+	query.
+		Size(5_000).
+		Sort(
+			es.Sort("modifiedDate").Order(order.Desc),
+			es.Sort("indexedAt").Order(order.Desc),
+		)
+
+	query.
+		Range("indexedAt").
+		GreaterThan("2020-06-01").
+		LesserThanOrEqual("now")
+
+	query.Aggs("DocumentIds",
+		es.AggTerms().
+			Field("document.id").
+			Size(250).
+			Aggs("OrderCounts",
+				es.AggMultiTerms().
+					Terms(
+						es.AggTerm("document.orders.count"),
+						es.AggTerm("files.order.count").
+							Missing("book.meta.author"),
+					),
+			),
+	)
+
+	marshal, err := json.Marshal(query)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
+}
+
+func createAggsQueryVanillaGo() string {
+	query := map[string]interface{}{
+		"size": 5000,
+		"sort": []map[string]interface{}{
+			{
+				"modifiedDate": map[string]interface{}{
+					"order": "desc",
+				},
+			},
+			{
+				"indexedAt": map[string]interface{}{
+					"order": "desc",
+				},
+			},
+		},
+		"query": map[string]interface{}{
+			"range": map[string]interface{}{
+				"indexedAt": map[string]interface{}{
+					"gt":  "2020-06-01",
+					"lte": "now",
+				},
+			},
+			"bool": map[string]interface{}{
+				"must": []map[string]interface{}{
+					{
+						"term": map[string]interface{}{
+							"type": "File",
+						},
+					},
+				},
+				"must_not": []map[string]interface{}{
+					{
+						"exists": map[string]interface{}{
+							"field": "file.name",
+						},
+					},
+				},
+			},
+		},
+		"aggs": map[string]interface{}{
+			"DocumentIds": map[string]interface{}{
+				"terms": map[string]interface{}{
+					"field": "document.id",
+					"size":  250,
+				},
+				"aggs": map[string]interface{}{
+					"OrderCounts": map[string]interface{}{
+						"multi_terms": map[string]interface{}{
+							"terms": []map[string]interface{}{
+								{
+									"field": "document.orders.count",
+								},
+								{
+									"field":   "files.order.count",
+									"missing": "book.meta.author",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	marshal, err := json.Marshal(query)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
+}
+
+func Test_Aggs_Queries_are_equal(t *testing.T) {
+	build := createAggsQuery()
+	pure := createAggsQueryVanillaGo()
+	assert.Equal(t, pure, build)
+}
+
+func Benchmark_Aggs_Example_Builder(b *testing.B) {
+	createAggsQuery()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		createAggsQuery()
+	}
+}
+
+func Benchmark_Aggs_Example_VanillaGo(b *testing.B) {
+	createAggsQueryVanillaGo()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		createAggsQueryVanillaGo()
+	}
+}
---
+++ benchmarks/ty_example_test.go
@@ -0,0 +1,91 @@
+package benchmarks_test
+
+import (
+	"encoding/json"
+	"github.com/GokselKUCUKSAHIN/es-query-builder/es"
+	"github.com/GokselKUCUKSAHIN/es-query-builder/test/assert"
+	"testing"
+)
+
+////    TY Example    ////
+
+func createTyExampleQuery(brandIds []int64, storefrontIds []string) string {
+	query := es.NewQuery(
+		es.Bool().
+			Filter(
+				es.Term("type", "LegalRule"),
+				es.TermsArray("brandId", brandIds),
+				es.TermsArray("allowedStorefronts.storefrontId", storefrontIds),
+			),
+	)
+	query.Size(1)
+	query.SourceFalse()
+
+	marshal, err := json.Marshal(query)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
+}
+
+func createTyExampleQueryVanillaGo(brandIds []int64, storefrontIds []string) string {
+	query := map[string]interface{}{
+		"size": 1,
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"filter": []interface{}{
+					map[string]interface{}{
+						"term": map[string]interface{}{
+							"type": "LegalRule",
+						},
+					},
+					map[string]interface{}{
+						"terms": map[string]interface{}{
+							"brandId": brandIds,
+						},
+					},
+					map[string]interface{}{
+						"terms": map[string]interface{}{
+							"allowedStorefronts.storefrontId": storefrontIds,
+						},
+					},
+				},
+			},
+		},
+		"_source": false,
+	}
+
+	marshal, err := json.Marshal(query)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
+}
+
+func Test_TyExample_Queries_are_equal(t *testing.T) {
+	brandIds := []int64{11, 22, 33, 44}
+	storefrontIds := []string{"35", "36", "43", "48", "49", "50"}
+	build := createTyExampleQuery(brandIds, storefrontIds)
+	pure := createTyExampleQueryVanillaGo(brandIds, storefrontIds)
+	assert.Equal(t, pure, build)
+}
+
+func Benchmark_Ty_Example_Builder(b *testing.B) {
+	brandIds := []int64{11, 22, 33, 44}
+	storefrontIds := []string{"35", "36", "43", "48", "49", "50"}
+	createTyExampleQuery(brandIds, storefrontIds)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		createTyExampleQuery(brandIds, storefrontIds)
+	}
+}
+
+func Benchmark_Ty_Example_VanillaGo(b *testing.B) {
+	brandIds := []int64{11, 22, 33, 44}
+	storefrontIds := []string{"35", "36", "43", "48", "49", "50"}
+	createTyExampleQueryVanillaGo(brandIds, storefrontIds)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		createTyExampleQueryVanillaGo(brandIds, storefrontIds)
+	}
+}
