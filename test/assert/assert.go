b38fdafd81e330ad9690a549ad78637d4ac218ed
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2024-09-01 01:34:06 +0300

    test: update assert functions with TestingT interface

--- test/assert/assert.go
+++ test/assert/assert.go
@@ -3,10 +3,13 @@ package assert
 import (
 	"encoding/json"
 	"reflect"
-	"testing"
 )
 
-func MarshalWithoutError(t *testing.T, body any) string {
+type TestingT interface {
+	Errorf(format string, args ...interface{})
+}
+
+func MarshalWithoutError(t TestingT, body any) string {
 	marshal, err := json.Marshal(body)
 	if err != nil {
 		t.Errorf("marshal error: '%s'", err.Error())
@@ -14,7 +17,7 @@ func MarshalWithoutError(t *testing.T, body any) string {
 	return string(marshal)
 }
 
-func Equal(t *testing.T, expected, actual any, messages ...string) {
+func Equal(t TestingT, expected, actual any, messages ...string) {
 	message := getMessage(messages)
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: '%v' type: <%s>, Actual: '%v' type: <%s>. %s",
@@ -22,7 +25,7 @@ func Equal(t *testing.T, expected, actual any, messages ...string) {
 	}
 }
 
-func EqualReference(t *testing.T, expected, actual any, messages ...string) {
+func EqualReference(t TestingT, expected, actual any, messages ...string) {
 	message := getMessage(messages)
 	expectedPointer, actualPointer := reflect.ValueOf(expected).Pointer(), reflect.ValueOf(actual).Pointer()
 	if !(expectedPointer == actualPointer) {
@@ -30,7 +33,7 @@ func EqualReference(t *testing.T, expected, actual any, messages ...string) {
 	}
 }
 
-func NotEqualReference(t *testing.T, expected, actual any, messages ...string) {
+func NotEqualReference(t TestingT, expected, actual any, messages ...string) {
 	message := getMessage(messages)
 	expectedPointer, actualPointer := reflect.ValueOf(expected).Pointer(), reflect.ValueOf(actual).Pointer()
 	if expectedPointer == actualPointer {
@@ -38,35 +41,35 @@ func NotEqualReference(t *testing.T, expected, actual any, messages ...string) {
 	}
 }
 
-func True(t *testing.T, condition bool, messages ...string) {
+func True(t TestingT, condition bool, messages ...string) {
 	message := getMessage(messages)
 	if !condition {
 		t.Errorf("Expected: true, Actual: false. %s", message)
 	}
 }
 
-func False(t *testing.T, condition bool, messages ...string) {
+func False(t TestingT, condition bool, messages ...string) {
 	message := getMessage(messages)
 	if condition {
 		t.Errorf("Expected: false, Actual: true. %s", message)
 	}
 }
 
-func Nil(t *testing.T, value any, messages ...string) {
+func Nil(t TestingT, value any, messages ...string) {
 	message := getMessage(messages)
 	if value != nil {
 		t.Errorf("Expected: nil, Actual: '%v'. %s", value, message)
 	}
 }
 
-func NotNil(t *testing.T, value any, messages ...string) {
+func NotNil(t TestingT, value any, messages ...string) {
 	message := getMessage(messages)
 	if value == nil {
 		t.Errorf("Expected: not nil, Actual: nil. %s", message)
 	}
 }
 
-func IsType(t *testing.T, expected, actual any, messages ...string) {
+func IsType(t TestingT, expected, actual any, messages ...string) {
 	message := getMessage(messages)
 	expectedType := reflect.TypeOf(expected)
 	actualValue := reflect.TypeOf(actual)
@@ -75,7 +78,7 @@ func IsType(t *testing.T, expected, actual any, messages ...string) {
 	}
 }
 
-func IsTypeString(t *testing.T, expectedType string, actual any, messages ...string) {
+func IsTypeString(t TestingT, expectedType string, actual any, messages ...string) {
 	message := getMessage(messages)
 	actualValue := reflect.TypeOf(actual)
 	if expectedType != actualValue.String() {
