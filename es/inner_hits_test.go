0c3979d52af8d13ad4a648c21e55fea86c928ccb
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2025-01-05 01:14:53 +0300

    test: add test for inner hits

--- es/inner_hits_test.go
+++ es/inner_hits_test.go
@@ -1,11 +1,14 @@
 package es_test
 
 import (
+	"testing"
+
 	"github.com/Trendyol/es-query-builder/es"
 	"github.com/Trendyol/es-query-builder/es/enums/sort/order"
 	"github.com/Trendyol/es-query-builder/test/assert"
-	"testing"
 )
+
+////   Inner Hits   ////
 
 func Test_InnerHits_should_exist_on_es_package(t *testing.T) {
 	// Given When Then
@@ -19,34 +22,6 @@ func Test_InnerHits_method_should_create_innerHitsType(t *testing.T) {
 	// Then
 	assert.NotNil(t, ih)
 	assert.IsTypeString(t, "es.innerHitsType", ih)
-}
-
-func Test_FieldCollapse_should_exist_on_es_package(t *testing.T) {
-	// Given When Then
-	assert.NotNil(t, es.FieldCollapse)
-}
-
-func Test_FieldCollapse_method_should_create_fieldCollapseType(t *testing.T) {
-	// Given
-	ih := es.FieldCollapse("name")
-
-	// Then
-	assert.NotNil(t, ih)
-	assert.IsTypeString(t, "es.fieldCollapseType", ih)
-}
-
-func Test_FieldAndFormat_should_exist_on_es_package(t *testing.T) {
-	// Given When Then
-	assert.NotNil(t, es.FieldAndFormat)
-}
-
-func Test_FieldAndFormat_method_should_create_fieldAndFormatType(t *testing.T) {
-	// Given
-	ih := es.FieldAndFormat("name")
-
-	// Then
-	assert.NotNil(t, ih)
-	assert.IsTypeString(t, "es.fieldAndFormatType", ih)
 }
 
 func Test_InnerHits_should_have_Explain_method(t *testing.T) {
@@ -285,4 +260,160 @@ func Test_InnerHits_Sort_should_create_json_with_sort_field_inside_inner_hits(t 
 	assert.NotNil(t, ih)
 	bodyJSON := assert.MarshalWithoutError(t, ih)
 	assert.Equal(t, "{\"sort\":[{\"indexedAt\":{\"order\":\"desc\"}}]}", bodyJSON)
+}
+
+func Test_InnerHits_should_have_Collapse_method(t *testing.T) {
+	// Given
+	ih := es.InnerHits()
+
+	// When Then
+	assert.NotNil(t, ih.Collapse)
+}
+
+func Test_InnerHits_Collapse_should_create_json_with_collapse_field_inside_inner_hits(t *testing.T) {
+	// Given
+	ih := es.InnerHits().Collapse(es.FieldCollapse("yelle"))
+	// When Then
+	assert.NotNil(t, ih)
+	bodyJSON := assert.MarshalWithoutError(t, ih)
+	assert.Equal(t, "{\"collapse\":{\"field\":\"yelle\"}}", bodyJSON)
+}
+
+func Test_InnerHits_should_have_DocvalueFields_method(t *testing.T) {
+	// Given
+	ih := es.InnerHits()
+
+	// When Then
+	assert.NotNil(t, ih.DocvalueFields)
+}
+
+func Test_InnerHits_DocvalueFields_should_create_json_with_docvalue_fields_field_inside_inner_hits(t *testing.T) {
+	// Given
+	ih := es.InnerHits().DocvalueFields(
+		es.FieldAndFormat("id"),
+		es.FieldAndFormat("name"),
+		es.FieldAndFormat("partition"),
+	)
+	// When Then
+	assert.NotNil(t, ih)
+	bodyJSON := assert.MarshalWithoutError(t, ih)
+	assert.Equal(t, "{\"docvalue_fields\":[{\"field\":\"id\"},{\"field\":\"name\"},{\"field\":\"partition\"}]}", bodyJSON)
+}
+
+////   Field Collapse   ////
+
+func Test_FieldCollapse_should_exist_on_es_package(t *testing.T) {
+	// Given When Then
+	assert.NotNil(t, es.FieldCollapse)
+}
+
+func Test_FieldCollapse_method_should_create_fieldCollapseType(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("name")
+
+	// Then
+	assert.NotNil(t, fc)
+	assert.IsTypeString(t, "es.fieldCollapseType", fc)
+}
+
+func Test_FieldCollapse_should_have_Collapse_method(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("id")
+
+	// When Then
+	assert.NotNil(t, fc.Collapse)
+}
+
+func Test_FieldCollapse_Collapse_should_create_json_with_collapse_field_inside_field_collapse(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("id").Collapse(es.FieldCollapse("index"))
+	// When Then
+	assert.NotNil(t, fc)
+	bodyJSON := assert.MarshalWithoutError(t, fc)
+	assert.Equal(t, "{\"collapse\":{\"field\":\"index\"},\"field\":\"id\"}", bodyJSON)
+}
+
+func Test_FieldCollapse_should_have_InnerHits_method(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("id")
+
+	// When Then
+	assert.NotNil(t, fc.InnerHits)
+}
+
+func Test_FieldCollapse_InnerHits_should_create_json_with_inner_hits_field_inside_field_collapse(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("display").InnerHits(es.InnerHits().Fields("name", "surname"))
+	// When Then
+	assert.NotNil(t, fc)
+	bodyJSON := assert.MarshalWithoutError(t, fc)
+	assert.Equal(t, "{\"field\":\"display\",\"inner_hits\":[{\"fields\":[\"name\",\"surname\"]}]}", bodyJSON)
+}
+
+func Test_FieldCollapse_should_have_MaxConcurrentGroupSearches_method(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("id")
+
+	// When Then
+	assert.NotNil(t, fc.MaxConcurrentGroupSearches)
+}
+
+// nolint:golint,lll
+func Test_FieldCollapse_MaxConcurrentGroupSearches_should_create_json_with_max_concurrent_group_searches_field_inside_field_collapse(t *testing.T) {
+	// Given
+	fc := es.FieldCollapse("display").MaxConcurrentGroupSearches(67)
+	// When Then
+	assert.NotNil(t, fc)
+	bodyJSON := assert.MarshalWithoutError(t, fc)
+	assert.Equal(t, "{\"field\":\"display\",\"max_concurrent_group_searches\":67}", bodyJSON)
+}
+
+////   Field And Format   ////
+
+func Test_FieldAndFormat_should_exist_on_es_package(t *testing.T) {
+	// Given When Then
+	assert.NotNil(t, es.FieldAndFormat)
+}
+
+func Test_FieldAndFormat_method_should_create_fieldAndFormatType(t *testing.T) {
+	// Given
+	fnf := es.FieldAndFormat("name")
+
+	// Then
+	assert.NotNil(t, fnf)
+	assert.IsTypeString(t, "es.fieldAndFormatType", fnf)
+}
+
+func Test_FieldAndFormat_should_have_Format_method(t *testing.T) {
+	// Given
+	fnf := es.FieldAndFormat("name")
+
+	// When Then
+	assert.NotNil(t, fnf.Format)
+}
+
+func Test_FieldAndFormat_Format_should_create_json_with_format_field_inside_field_and_format(t *testing.T) {
+	// Given
+	fnf := es.FieldAndFormat("xyz").Format("yyyy-MM-dd")
+	// When Then
+	assert.NotNil(t, fnf)
+	bodyJSON := assert.MarshalWithoutError(t, fnf)
+	assert.Equal(t, "{\"field\":\"xyz\",\"format\":\"yyyy-MM-dd\"}", bodyJSON)
+}
+
+func Test_FieldAndFormat_should_have_IncludeUnmapped_method(t *testing.T) {
+	// Given
+	fnf := es.FieldAndFormat("name")
+
+	// When Then
+	assert.NotNil(t, fnf.IncludeUnmapped)
+}
+
+func Test_FieldAndFormat_IncludeUnmapped_should_create_json_with_include_unmapped_field_inside_field_and_format(t *testing.T) {
+	// Given
+	fnf := es.FieldAndFormat("hello").IncludeUnmapped(false)
+	// When Then
+	assert.NotNil(t, fnf)
+	bodyJSON := assert.MarshalWithoutError(t, fnf)
+	assert.Equal(t, "{\"field\":\"hello\",\"include_unmapped\":false}", bodyJSON)
 }
