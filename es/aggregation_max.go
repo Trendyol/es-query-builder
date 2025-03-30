67396c94b96412e926d1a04dfce0d30c58550e14
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2025-03-31 01:25:33 +0300

    feature: aggregation_max implemented

---
+++ es/aggregation_max.go
@@ -0,0 +1,140 @@
+package es
+
+type maxAggType Object
+
+// MaxAgg creates a max aggregation for the given field.
+//
+// The max aggregation calculates the maximum value of a specified numeric field.
+//
+// Example usage:
+//
+//	agg := es.MaxAgg("price")
+//	// agg now contains an es.maxAggType object that calculates the maximum value for the "price" field.
+//
+// Parameters:
+//   - field: The field for which the maximum value should be computed.
+//
+// Returns:
+//
+//	An es.maxAggType object representing the max aggregation.
+func MaxAgg(field string) maxAggType {
+	return maxAggType{
+		"max": Object{
+			"field": field,
+		},
+	}
+}
+
+// Missing sets the "missing" value for the max aggregation.
+//
+// This value is used when documents do not have a value for the specified field. It allows missing values
+// to be treated as a specific number instead of being ignored.
+//
+// Example usage:
+//
+//	agg := es.MaxAgg("price").Missing(0)
+//	// agg now contains an es.maxAggType object that treats missing values as 0.
+//
+// Parameters:
+//   - missing: The value to use when a document lacks a field value.
+//
+// Returns:
+//
+//	An es.maxAggType object with the "missing" field set.
+func (max maxAggType) Missing(missing any) maxAggType {
+	return max.putInTheField("missing", missing)
+}
+
+// Script sets a script for the max aggregation instead of using a field value.
+//
+// This allows the aggregation to be computed based on a script, rather than directly referencing
+// a field in the document.
+//
+// Example usage:
+//
+//	agg := es.MaxAgg("price").Script(es.ScriptSource("doc['price'].value * 1.2", ScriptLanguage.Painless))
+//	// agg now contains an es.maxAggType object that applies a script for computing the values.
+//
+// Parameters:
+//   - script: A script to calculate values dynamically.
+//
+// Returns:
+//
+//	An es.maxAggType object with the "script" field set.
+func (max maxAggType) Script(script scriptType) maxAggType {
+	return max.putInTheField("script", script)
+}
+
+// Format sets the output format for the max aggregation.
+//
+// This is used to specify how the results should be formatted when returned.
+//
+// Example usage:
+//
+//	agg := es.MaxAgg("price").Format("000.0")
+//	// agg now contains an es.maxAggType object with a defined number format.
+//
+// Parameters:
+//   - format: A string specifying the output format.
+//
+// Returns:
+//
+//	An es.maxAggType object with the "format" field set.
+func (max maxAggType) Format(format string) maxAggType {
+	return max.putInTheField("format", format)
+}
+
+// Meta sets a custom metadata field for the max aggregation.
+//
+// This allows additional information to be attached to the aggregation result for tracking or processing.
+//
+// Example usage:
+//
+//	agg := es.MaxAgg("price").Meta("source", "sales_data")
+//	// agg now contains an es.maxAggType object with metadata indicating the source is "sales_data".
+//
+// Parameters:
+//   - key: The metadata key.
+//   - value: The metadata value.
+//
+// Returns:
+//
+//	An es.maxAggType object with the "meta" field set.
+func (max maxAggType) Meta(key string, value any) maxAggType {
+	meta, exists := getFieldFromAggs(max, "max", "meta")
+	if !exists {
+		meta = Object{}
+	}
+	meta[key] = value
+	return max.putInTheField("meta", meta)
+}
+
+// Aggs adds sub-aggregations to the max aggregation.
+//
+// This allows performing additional calculations on top of the max aggregation.
+//
+// Example usage:
+//
+//	agg := es.MaxAgg("price").Aggs(es.Agg("price_distribution", es.HistogramAgg("price", 10)))
+//	// agg now contains an es.maxAggType object with a histogram sub-aggregation.
+//
+// Parameters:
+//   - aggs: A variadic list of aggsType representing the sub-aggregations.
+//
+// Returns:
+//
+//	An es.maxAggType object with the specified sub-aggregations added.
+func (max maxAggType) Aggs(aggs ...aggsType) maxAggType {
+	if len(aggs) == 1 && aggs[0] == nil {
+		return max
+	}
+	max["aggs"] = reduceAggs(aggs...)
+	return max
+}
+
+func (max maxAggType) putInTheField(key string, value any) maxAggType {
+	if maxAgg, ok := max["max"].(Object); ok {
+		maxAgg[key] = value
+	}
+	return max
+}
---
+++ es/aggregation_max_test.go
@@ -0,0 +1,150 @@
+package es_test
+
+import (
+	ScriptLanguage "github.com/Trendyol/es-query-builder/es/enums/script-language"
+	"testing"
+
+	"github.com/Trendyol/es-query-builder/es"
+	"github.com/Trendyol/es-query-builder/test/assert"
+)
+
+func Test_MaxAgg_should_exist_on_es_package(t *testing.T) {
+	t.Parallel()
+	// Given When Then
+	assert.NotNil(t, es.MaxAgg)
+}
+
+func Test_MaxAgg_should_return_type_of_maxAggType(t *testing.T) {
+	t.Parallel()
+	// Given
+	aggsQuery := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, aggsQuery)
+	assert.IsTypeString(t, "es.maxAggType", aggsQuery)
+	assert.MarshalWithoutError(t, aggsQuery)
+}
+
+func Test_MaxAgg_should_create_json_with_terms_field_inside(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"max\":{\"field\":\"price\"}}", bodyJSON)
+}
+
+func Test_MaxAgg_should_have_Missing_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Missing)
+}
+
+func Test_Missing_should_add_missing_field_into_MaxAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price").Missing("missing_name")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"max\":{\"field\":\"price\",\"missing\":\"missing_name\"}}", bodyJSON)
+}
+
+func Test_MaxAgg_should_have_Script_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Script)
+}
+
+func Test_Script_should_add_script_field_into_MaxAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price").Script(es.ScriptID("id_12345", ScriptLanguage.Painless))
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"max\":{\"field\":\"price\",\"script\":{\"id\":\"id_12345\",\"lang\":\"painless\"}}}", bodyJSON)
+}
+
+func Test_MaxAgg_should_have_Format_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Format)
+}
+
+func Test_Format_should_add_format_field_into_MaxAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price").Format("#.00")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"max\":{\"field\":\"price\",\"format\":\"#.00\"}}", bodyJSON)
+}
+
+func Test_MaxAgg_should_have_Meta_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Meta)
+}
+
+func Test_Meta_should_add_meta_field_into_MaxAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price").
+		Meta("k1", "v1").
+		Meta("k2", "v2")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"max\":{\"field\":\"price\",\"meta\":{\"k1\":\"v1\",\"k2\":\"v2\"}}}", bodyJSON)
+}
+
+func Test_MaxAgg_should_have_Aggs_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Aggs)
+}
+
+func Test_Aggs_should_add_aggs_field_into_MaxAgg_when_not_empty(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price").Aggs(es.Agg("max_stock", es.MaxAgg("stock")))
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"aggs\":{\"max_stock\":{\"max\":{\"field\":\"stock\"}}},\"max\":{\"field\":\"price\"}}", bodyJSON)
+}
+
+func Test_Aggs_should_not_add_aggs_field_into_MaxAgg_when_it_is_empty(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MaxAgg("price").Aggs(nil)
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"max\":{\"field\":\"price\"}}", bodyJSON)
+}
