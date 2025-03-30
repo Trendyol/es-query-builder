33bdd0f9843012e583e8b9ee4d29e56d5abd98f9
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2025-03-31 01:25:39 +0300

    feature: aggregation_min implemented

---
+++ es/aggregation_min.go
@@ -0,0 +1,140 @@
+package es
+
+type minAggType Object
+
+// MinAgg creates a min aggregation for the given field.
+//
+// The min aggregation calculates the minimum value of a specified numeric field.
+//
+// Example usage:
+//
+//	agg := es.MinAgg("price")
+//	// agg now contains an es.minAggType object that calculates the minimum value for the "price" field.
+//
+// Parameters:
+//   - field: The field for which the minimum value should be computed.
+//
+// Returns:
+//
+//	An es.minAggType object representing the min aggregation.
+func MinAgg(field string) minAggType {
+	return minAggType{
+		"min": Object{
+			"field": field,
+		},
+	}
+}
+
+// Missing sets the "missing" value for the min aggregation.
+//
+// This value is used when documents do not have a value for the specified field. It allows missing values
+// to be treated as a specific number instead of being ignored.
+//
+// Example usage:
+//
+//	agg := es.MinAgg("price").Missing(0)
+//	// agg now contains an es.minAggType object that treats missing values as 0.
+//
+// Parameters:
+//   - missing: The value to use when a document lacks a field value.
+//
+// Returns:
+//
+//	An es.minAggType object with the "missing" field set.
+func (min minAggType) Missing(missing any) minAggType {
+	return min.putInTheField("missing", missing)
+}
+
+// Script sets a script for the min aggregation instead of using a field value.
+//
+// This allows the aggregation to be computed based on a script, rather than directly referencing
+// a field in the document.
+//
+// Example usage:
+//
+//	agg := es.MinAgg("price").Script(es.ScriptSource("doc['price'].value * 1.2", ScriptLanguage.Painless))
+//	// agg now contains an es.minAggType object that applies a script for computing the values.
+//
+// Parameters:
+//   - script: A script to calculate values dynamically.
+//
+// Returns:
+//
+//	An es.minAggType object with the "script" field set.
+func (min minAggType) Script(script scriptType) minAggType {
+	return min.putInTheField("script", script)
+}
+
+// Format sets the output format for the min aggregation.
+//
+// This is used to specify how the results should be formatted when returned.
+//
+// Example usage:
+//
+//	agg := es.MinAgg("price").Format("000.0")
+//	// agg now contains an es.minAggType object with a defined number format.
+//
+// Parameters:
+//   - format: A string specifying the output format.
+//
+// Returns:
+//
+//	An es.minAggType object with the "format" field set.
+func (min minAggType) Format(format string) minAggType {
+	return min.putInTheField("format", format)
+}
+
+// Meta sets a custom metadata field for the min aggregation.
+//
+// This allows additional information to be attached to the aggregation result for tracking or processing.
+//
+// Example usage:
+//
+//	agg := es.MinAgg("price").Meta("source", "sales_data")
+//	// agg now contains an es.minAggType object with metadata indicating the source is "sales_data".
+//
+// Parameters:
+//   - key: The metadata key.
+//   - value: The metadata value.
+//
+// Returns:
+//
+//	An es.minAggType object with the "meta" field set.
+func (min minAggType) Meta(key string, value any) minAggType {
+	meta, exists := getFieldFromAggs(min, "min", "meta")
+	if !exists {
+		meta = Object{}
+	}
+	meta[key] = value
+	return min.putInTheField("meta", meta)
+}
+
+// Aggs adds sub-aggregations to the min aggregation.
+//
+// This allows performing additional calculations on top of the min aggregation.
+//
+// Example usage:
+//
+//	agg := es.MinAgg("price").Aggs(es.Agg("price_distribution", es.HistogramAgg("price", 10)))
+//	// agg now contains an es.minAggType object with a histogram sub-aggregation.
+//
+// Parameters:
+//   - aggs: A variadic list of aggsType representing the sub-aggregations.
+//
+// Returns:
+//
+//	An es.minAggType object with the specified sub-aggregations added.
+func (min minAggType) Aggs(aggs ...aggsType) minAggType {
+	if len(aggs) == 1 && aggs[0] == nil {
+		return min
+	}
+	min["aggs"] = reduceAggs(aggs...)
+	return min
+}
+
+func (min minAggType) putInTheField(key string, value any) minAggType {
+	if minAgg, ok := min["min"].(Object); ok {
+		minAgg[key] = value
+	}
+	return min
+}
---
+++ es/aggregation_min_test.go
@@ -0,0 +1,150 @@
+package es_test
+
+import (
+	ScriptLanguage "github.com/Trendyol/es-query-builder/es/enums/script-language"
+	"testing"
+
+	"github.com/Trendyol/es-query-builder/es"
+	"github.com/Trendyol/es-query-builder/test/assert"
+)
+
+func Test_MinAgg_should_exist_on_es_package(t *testing.T) {
+	t.Parallel()
+	// Given When Then
+	assert.NotNil(t, es.MinAgg)
+}
+
+func Test_MinAgg_should_return_type_of_minAggType(t *testing.T) {
+	t.Parallel()
+	// Given
+	aggsQuery := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, aggsQuery)
+	assert.IsTypeString(t, "es.minAggType", aggsQuery)
+	assert.MarshalWithoutError(t, aggsQuery)
+}
+
+func Test_MinAgg_should_create_json_with_terms_field_inside(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"min\":{\"field\":\"price\"}}", bodyJSON)
+}
+
+func Test_MinAgg_should_have_Missing_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Missing)
+}
+
+func Test_Missing_should_add_missing_field_into_MinAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price").Missing("missing_name")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"min\":{\"field\":\"price\",\"missing\":\"missing_name\"}}", bodyJSON)
+}
+
+func Test_MinAgg_should_have_Script_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Script)
+}
+
+func Test_Script_should_add_script_field_into_MinAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price").Script(es.ScriptID("id_12345", ScriptLanguage.Painless))
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"min\":{\"field\":\"price\",\"script\":{\"id\":\"id_12345\",\"lang\":\"painless\"}}}", bodyJSON)
+}
+
+func Test_MinAgg_should_have_Format_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Format)
+}
+
+func Test_Format_should_add_format_field_into_MinAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price").Format("#.00")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"min\":{\"field\":\"price\",\"format\":\"#.00\"}}", bodyJSON)
+}
+
+func Test_MinAgg_should_have_Meta_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Meta)
+}
+
+func Test_Meta_should_add_meta_field_into_MinAgg(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price").
+		Meta("k1", "v1").
+		Meta("k2", "v2")
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"min\":{\"field\":\"price\",\"meta\":{\"k1\":\"v1\",\"k2\":\"v2\"}}}", bodyJSON)
+}
+
+func Test_MinAgg_should_have_Aggs_method(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price")
+
+	// When Then
+	assert.NotNil(t, a.Aggs)
+}
+
+func Test_Aggs_should_add_aggs_field_into_MinAgg_when_not_empty(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price").Aggs(es.Agg("min_stock", es.MinAgg("stock")))
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"aggs\":{\"min_stock\":{\"min\":{\"field\":\"stock\"}}},\"min\":{\"field\":\"price\"}}", bodyJSON)
+}
+
+func Test_Aggs_should_not_add_aggs_field_into_MinAgg_when_it_is_empty(t *testing.T) {
+	t.Parallel()
+	// Given
+	a := es.MinAgg("price").Aggs(nil)
+
+	// When Then
+	assert.NotNil(t, a)
+	bodyJSON := assert.MarshalWithoutError(t, a)
+	assert.Equal(t, "{\"min\":{\"field\":\"price\"}}", bodyJSON)
+}
