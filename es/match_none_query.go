af83757257af1a6e39440225334db8cb04f22152
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2026-07-24 22:58:52 +0300

    fix: update match none query's parameter

--- es/match_none_query.go
+++ es/match_none_query.go
@@ -2,43 +2,34 @@ package es
 
 type matchNoneType Object
 
-// MatchNone creates a new es.matchNoneType object with the specified field and query.
+// MatchNone creates a new es.matchNoneType object that matches no documents.
 //
-// This function initializes an es.matchNoneType object for a match_none query, where the key
-// represents the field name and query is the value to be matched. This is used to construct
-// queries that explicitly match no documents for the specified value in the given field.
+// This function initializes an es.matchNoneType object that is used to construct queries
+// that match no documents. This can be useful for scenarios where you want to explicitly
+// return an empty result set.
 //
 // Example usage:
 //
-//	mn := es.MatchNone("title", "es-query-builder")
-//	// mn now contains an es.matchNoneType object that matches no documents for the "title" field with the query "es-query-builder".
-//
-// Parameters:
-//   - key: A string representing the field name for the match_none query.
-//   - query: The value to be used in the match_none query. The type is generic.
+//	mn := es.MatchNone()
+//	// mn now contains an es.matchNoneType object that matches no documents.
 //
 // Returns:
 //
-//	An es.matchNoneType object containing the specified match_none query.
-func MatchNone[T any](key string, query T) matchNoneType {
+//	An es.matchNoneType object with a match_none query that matches no documents.
+func MatchNone() matchNoneType {
 	return matchNoneType{
-		"match_none": Object{
-			key: Object{
-				"query": query,
-			},
-		},
+		"match_none": Object{},
 	}
 }
 
 // Boost sets the "boost" field in the match_none query.
 //
 // This method configures the match_none query to use a specified boost factor, which influences
-// the relevance scoring of the matched documents. It calls putInTheField to add or update
-// the "boost" key in the match_none query object.
+// the relevance scoring of the matched documents.
 //
 // Example usage:
 //
-//	mn := es.MatchNone("title", "es-query-builder").Boost(1.5)
+//	mn := es.MatchNone().Boost(1.5)
 //	// mn now has a "boost" field set to 1.5 in the match_none query object.
 //
 // Parameters:
@@ -52,5 +43,5 @@ func (m matchNoneType) Boost(boost float64) matchNoneType {
 }
 
 func (m matchNoneType) putInTheField(key string, value any) matchNoneType {
-	return genericPutInTheFieldOfFirstChild(m, "match_none", key, value)
+	return genericPutInTheField(m, "match_none", key, value)
 }
